1c593873f48ce25796c0bfb8f5bbceb7cf79b299
Diego Bouhid <admin@draftklub.com>
2026-04-24 00:56:15 -0300

    fix: import IdentityModule in KlubModule to resolve FirebaseAuthGuard DI

--- apps/api/src/modules/klub/klub.module.ts
+++ apps/api/src/modules/klub/klub.module.ts
@@ -5,8 +5,10 @@ import { GetKlubByIdHandler } from './application/queries/get-klub-by-id.handler
 import { ListKlubsHandler } from './application/queries/list-klubs.handler';
 import { KlubFacade } from './public/klub.facade';
 import { KlubController } from './api/klub.controller';
+import { IdentityModule } from '../identity/identity.module';
 
 @Module({
+  imports: [IdentityModule],
   controllers: [KlubController],
   providers: [
     KlubPrismaRepository,
